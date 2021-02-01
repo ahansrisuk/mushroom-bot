a833fca644e08dafc03b1e0c4841ff8c2fb53b5f
James Jung <thisisjamesj@gmail.com>
2021-02-01 15:48:17 -0500

    clem

--- memes/asciiArt.js
+++ memes/asciiArt.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 function sendAsciiArt(channel, file){
     fs.readFile(file, 'utf16le', (err, data) => { 
         if (err) throw err; 
+        console.log(data.toString().length);
         channel.send("```" + data.toString() + "```");
     }) 
 }
